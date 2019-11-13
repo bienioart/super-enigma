67e30e815ae0b177135701d679988c5cbcb6a934
bienioart <bienioart@gmail.com>
2019-11-13 17:02:51 +0100

    work

---
+++ password/main.js
@@ -0,0 +1,28 @@
+const pole = document.getElementById('pass');
+const tekst = document.querySelector('div');
+const haslo = "1234";
+const wiadomosc = "uzyskano dostęp";
+
+pole.addEventListener('input', (e) => {
+
+    if (haslo === e.target.value) {
+
+        tekst.textContent = wiadomosc;
+        e.target.value = '';}
+    
+        
+        
+    else{
+
+        tekst.textContent = "Wpisz poprawne haslo aby uzyskac dostep";
+    }
+    
+
+})
+
+
+
+
+pole.addEventListener('focus', ()=> {
+    pole.classList.add('active');
+})
