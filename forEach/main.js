27daaeed884d236813f4f0f531708ca38d6d9113
bienioart <bienioart@gmail.com>
2019-11-13 09:57:29 +0100

    ...

---
+++ forEach/main.js
@@ -0,0 +1,20 @@
+
+const btn = document.querySelector('button');
+const list = document.querySelectorAll('li');
+let size = 30;
+
+btn.addEventListener('click', () => {
+
+    size++;
+
+    list.forEach( (elo) => {
+        elo.style.display = 'block';
+        elo.style.fontSize = size + 'px';
+        
+    })
+
+
+})
+
+
+
--- minilistjs/main.js
+++ minilistjs/main.js
@@ -3,20 +3,8 @@ const btn = document.querySelector('button');
 const list = document.querySelectorAll('li');
 let fontSize = 10;
 
-btn.addEventListener('click',() => {
-
-    console.log('dziala');
-
-    fontSize++;
-    
-    for (let i = 0; i < list.length; i++) {
-    list[i].style.display = 'block';
-    list[i].style.fontSize = fontSize + 'px';
+list.forEach(function(element, index, array) {
+    console.log(element);
     
 
-
-}
-
-
-
-})
+});
