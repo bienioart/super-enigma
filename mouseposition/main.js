577c6b9b328bcb02740102c8e2f3f3f957a9bf5a
bienioart <bienioart@gmail.com>
2019-11-13 11:03:59 +0100

    next

--- mouseposition/main.js
+++ mouseposition/main.js
@@ -2,4 +2,10 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', (e) => {
   h1.innerHTML = (e.screenX) + "," + (e.screenY);
+
+  document.body.style.backgroundColor = `rgb(${e.screenX}, ${e.screenY} , 0)`;
+
+  // wstrzykiwanie ważne nie używać " " tylko ````` !!!!!  ` $ {}. oraz wiadome funkcja strzałkowa i e jako event. event pobiera wlasciwosci z js.
+
 })
+
