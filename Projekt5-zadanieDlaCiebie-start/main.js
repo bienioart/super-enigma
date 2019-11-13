788b5d88101cc97624c3da2f10c80f898073111c
bienioart <bienioart@gmail.com>
2019-11-13 17:43:31 +0100

    dziala

--- Projekt5-zadanieDlaCiebie-start/main.js
+++ Projekt5-zadanieDlaCiebie-start/main.js
@@ -7,11 +7,28 @@ const passwords = ['jedEN', 'DwA'];
 const messages = ["super", "działa!"]
 
 const showMessage = (e) => {
+
+
+    const show = document.querySelector('div');
+   
+    
+
+    if (`${e.target.value}` === passwords[0].toLowerCase()) {
+
+        show.textContent = messages[0];
+    }
+
+    if (`${e.target.value}` === passwords[1].toLowerCase()) {
+
+        show.textContent = messages[1];
+    }
+
+
+
+
+
  //tutaj rozwiązanie
  // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
 }
 
 input.addEventListener("input", showMessage)
-
-
-
