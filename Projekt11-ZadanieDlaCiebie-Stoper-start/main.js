7ae704e2501dfadf7862543fd117cdf18682d1d5
Mateusz Bien <mateuszbien@yahoo.com>
2019-11-25 20:50:49 +0100

    end

---
+++ Projekt11-ZadanieDlaCiebie-Stoper-start/main.js
@@ -0,0 +1,43 @@
+// http://websamuraj.pl/examples/js/projekt11/
+
+const start = document.querySelector('.main');
+const reset = document.querySelector('.reset');
+let timer = new Date();
+const diver = document.querySelector('body > div > div');
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+
+
+function actionStart() {
+    setTimeout(actionStart, 1000);
+    diver.innerHTML = hours + ":" + minutes + ":" + seconds;
+    seconds++;
+    if (seconds >= 60) {
+        minutes++;
+        seconds = 0;
+    }
+    if (minutes >= 60) {
+        hours++;
+        minutes = 0;
+    }
+}
+
+
+
+function actionReset() {
+    clearTimeout(actionStart);
+    diver.textContent = "---";
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+}
+
+
+
+
+
+
+start.addEventListener('click', actionStart);
+reset.addEventListener('click', actionReset);
