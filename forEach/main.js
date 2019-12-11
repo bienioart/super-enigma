e8bf42117e2b3f8afe2559816696d03d78e22427
Mateusz Bien <mateuszbien@yahoo.com>
2019-12-11 16:32:48 +0100

    loop and forEach

--- forEach/main.js
+++ forEach/main.js
@@ -1,11 +1,29 @@
+// pętla
+
+// const btn = document.querySelector('button');
+// const list = document.querySelectorAll('li');
+// const ul = document.querySelector('ul');
+// let size = 10;
+
+
+// btn.addEventListener('click', () => {
+//     size++;
+//     for (let i = 0; i < list.length; i++)
+//         list[i].style.display = 'block';
+//     ul.style.fontSize = size + 'px';
+// })
+
+// forEach
+
 const btn = document.querySelector('button');
 const list = document.querySelectorAll('li');
-let size = 30;
+let size = 10;
+
 
 btn.addEventListener('click', () => {
     size++;
-    list.forEach((elo) => {
-        elo.style.display = 'block';
-        elo.style.fontSize = size + 'px';
+    list.forEach((loop) => {
+        loop.style.display = 'block';
+        loop.style.fontSize = size + 'px';
     })
 })
