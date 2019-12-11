e840a27bae9ccad88cba50ea037e51c267ca59de
Mateusz Bien <mateuszbien@yahoo.com>
2019-12-11 15:13:03 +0100

    correcttion

--- App-Losuj-Imie/main.js
+++ App-Losuj-Imie/main.js
@@ -1,28 +1,19 @@
+// Draw a name for your baby!
+
 const btn = document.querySelector('button');
 const div = document.querySelector('div');
 
-
 const names = ["Jagienka", "Dobrawa (Dobrusia)", "Daria", "Asia", "Kira", "Marysia", "Anastazja", "Kasia"];
 
-const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę", "Szczerze uważam", "Na 100% twierdzę", "Jestem pewien"];
+const prefixs = ["Wydaje mi się", "Mam wrażenie", "Myślę że", "Szczerze uważam", "Na 100% twierdzę", "Jestem pewien"];
 
 function start(){
-
-
     const indexPrefixs = Math.floor(Math.random() * prefixs.length);
- 
     const indexNames = Math.floor(Math.random() * names.length);
 
-    console.log(indexPrefixs);
-    console.log(indexNames);
-    
-    
+    // console.log(indexPrefixs);
+    // console.log(indexNames);
 
-
-    div.textContent = prefixs[indexNames] + " że " + names[indexPrefixs];
-    
-    
+    div.textContent = prefixs[indexNames - 1] + " że " + names[indexPrefixs - 1];
 }
-
-
 btn.addEventListener('click', start);
