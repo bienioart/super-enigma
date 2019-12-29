49c0cb72e129abd5926eeb55ffc652eb38b1f2b6
Mateusz Bien <mateuszbien@yahoo.com>
2019-12-29 13:03:41 +0100

    lessons

---
+++ react-od-podstaw-pusty/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+return (
+    <div className="App">
+      <div>1</div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default App;
