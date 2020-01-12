9f038d2195a16890dd454739f0b87f073b7f410f
Mateusz Bien <mateuszbien@yahoo.com>
2020-01-12 18:48:01 +0100

    education

--- REACT/imput-tekst/App.js
+++ REACT/imput-tekst/App.js
@@ -1,12 +1,20 @@
 class App extends React.Component {
   state = {
-    counter: 0
+    value: ""
   }
+
+  handleChange(e){
+    console.log(e.target.value);
+    
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
+     <React.Fragment>
+       <input onChange={this.handleChange} type="text" />
+      <button>Reset</button>
+      <h1 className="title"></h1>
+     </React.Fragment>
     );
   }
 }
